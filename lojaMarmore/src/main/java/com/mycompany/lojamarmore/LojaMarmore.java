e5e7d83d506049d066c000cfb6ef93a47b326a9a
mayconmycklin <142271259+mayconmycklin@users.noreply.github.com>
2023-10-23 17:52:35 -0300

    Add files via upload

---
+++ lojaMarmore/src/main/java/com/mycompany/lojamarmore/LojaMarmore.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lojamarmore;
+
+/**
+ *
+ * @author maycon.mssouza
+ */
+public class LojaMarmore {
+
+    public static void main(String[] args) {
+       LoginLoja novaTela  = new LoginLoja();
+       novaTela.setVisible(true);
+    }
+}
